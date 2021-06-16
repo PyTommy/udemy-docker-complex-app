6e24c91755c50f32833a27930174ec21beaf7702
Hiroki Tominaga <hirokitominaga@Hirokis-MacBook-Pro.local>
2021-06-16 15:20:46 +0900

    Modfied react test to pass

--- client/src/App.js
+++ client/src/App.js
@@ -2,44 +2,47 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import OtherPage from './OtherPage';
 import Fib from './Fib';
+import ErrorBoundary from './ErrorBoundary';
 
 function App() {
 	return (
-		<Router>
-			<div
-				style={{
-					position: 'absolute',
-					top: '30px',
-					left: '50%',
-					width: '80%',
-					height: 'auto',
-					border: '1px solid',
-					padding: 10,
-					transform: 'translate(-50%)',
-					display: 'flex',
-					justifyContent: 'center',
-					justifyItems: 'center',
-					flexDirection: 'column',
-				}}
-			>
-				<div style={{ marginBottom: 20 }}>
-					<Route exact path="/" component={Fib} />
-					<Route path="/otherpage" component={OtherPage} />
+		<ErrorBoundary>
+			<Router>
+				<div
+					style={{
+						position: 'absolute',
+						top: '30px',
+						left: '50%',
+						width: '80%',
+						height: 'auto',
+						border: '1px solid',
+						padding: 10,
+						transform: 'translate(-50%)',
+						display: 'flex',
+						justifyContent: 'center',
+						justifyItems: 'center',
+						flexDirection: 'column',
+					}}
+				>
+					<div style={{ marginBottom: 20 }}>
+						<Route exact path="/" component={Fib} />
+						<Route path="/otherpage" component={OtherPage} />
+					</div>
+					<div>
+						<ul>
+							<li>
+								{' '}
+								<Link to="/">Home</Link>
+							</li>
+							<li>
+								{' '}
+								<Link to="/otherpage">Other Page</Link>
+							</li>
+						</ul>
+					</div>
 				</div>
-				<div>
-					<ul>
-						<li>
-							{' '}
-							<Link to="/">Home</Link>
-						</li>
-						<li>
-							{' '}
-							<Link to="/otherpage">Other Page</Link>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</Router>
+			</Router>
+		</ErrorBoundary>
 	);
 }
 
